924b98c3407534428912c2d588bc91fbd453fe77
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2019-01-28 17:24:42 +0200

    feat(tests): add example how to reproduce #/TheBrainFamily/cypress-cucumber-preprocessor/issues/112 with new version

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,31 +14,15 @@
 const cucumber = require('cypress-cucumber-preprocessor').default;
 const webpack = require('@cypress/webpack-preprocessor');
 
-const webpackOptions = {
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: [/node_modules/],
-                use: [
-                    {
-                        loader: 'ts-loader'
-                    }
-                ]
-            }
-        ]
-    }
-};
-
 module.exports = (on) => {
+    const options = {
+        webpackOptions: require("./webpack.config.js")
+    }
     on('file:preprocessor', (file) => {
         if (file.filePath.match(/\.(js|jsx)/g)) {
             console.log('111111');
             console.log(file.filePath);
-        return webpack(webpackOptions)(file)
+        return webpack(options)(file)
     } else {
         console.log('222222');
         console.log(file.filePath);
